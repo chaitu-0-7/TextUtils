fcb511a6b0b4d0de55fc571bec1233efeed34129
Yadavalli Chaitanya <ychaitu2@gmail.com>
2023-04-20 00:58:46 +0530

    Routing is implemented

--- TextUtils/text-utils/src/App.js
+++ TextUtils/text-utils/src/App.js
@@ -1,32 +1,55 @@
-// import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
 import TextForm from './components/textForm';
-import About from './components/About';
 import { useState } from 'react';
+import About from './components/About';
+import Alert from './components/Alert';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   const [mode, setMode]=useState('light')
+  const [alert,setAlert]=useState(null)
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      typ:type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1000);
+  }
   const toggleMode=()=>{
     if(mode==='light'){
       setMode('dark')
       document.body.style.backgroundColor='#042743'
+      showAlert('Dark mode has been enabled','Success')
     }
     else{
       setMode('light')
       document.body.style.backgroundColor='white'
+      showAlert('Light mode has been enabled','Success')
     }
   }
   return (
     <>
-    <Navbar title='Text Utils' mode={mode} toggleMode={toggleMode}/>
-    {/* <Navbar/> */}
-    <div className='container'>
-    <TextForm heading='Enter your text' mode={mode}/>
-    </div>
+     <Router>
+      <Navbar title='Text Utils' mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
+ 
+    <Routes>
+      <Route exact path="/" element={
+            <div className='container'>
+       <TextForm heading='Please enter your text' mode={mode}/>
+    </div>} />
+      <Route path="/about" element={<About/>} />
+    </Routes>
+  </Router>
+      
+    
 
     </>
   );
-}
+};
 
 export default App;
--- TextUtils/text-utils/src/components/navbar.js
+++ TextUtils/text-utils/src/components/navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 // Proptypes are for defining the parameters you pass during to the function it's always better to set the proptypes before itself 
 export default function navbar(props){
   const dic={
@@ -10,23 +11,24 @@ export default function navbar(props){
         <div>
           <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
             <div className="container-fluid">
-              <a className="navbar-brand" href="/">{props.title}</a>
+              <Link to='/' className="navbar-brand" >{props.title}</Link>
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
               </button>
               <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mb-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                <a className="nav-link active" href="/">Features</a>
+                <Link to='/' className="nav-link active" >Features</Link>
                 </li>
                 <li className="nav-item">
-                <a className="nav-link active" href='/'>About</a>
+                <Link to="/about" className="nav-link active" >About</Link>
+                {/* Have to use link to stop page reload */}
                 </li>
                 </ul>
-                <div className={`form-check form-switch text-${dic[props.mode]}`}>
+                <li className={`form-check form-switch text-${dic[props.mode]}`}>
                   <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.toggleMode}/>
                   <label className="form-check-label" htmlFor="flexSwitchCheckDefault" >Switch to {dic[props.mode]} Mode</label>
-                </div>
+                </li>
               </div>
             </div>
           </nav>
